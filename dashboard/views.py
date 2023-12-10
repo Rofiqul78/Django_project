b37726cfc0c9cb310edc269f657a70b89b879338
Rofiqul78 <alamewu@gmail.com>
2023-12-11 01:03:07 +0600

    get all table data

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from blogs.models import BlogTables
 
 @login_required(login_url='/')
 def dashboard(request):
-    return render(request,'dashboard_index.html')
+    querydata =BlogTables.objects.all()
+    context = {
+   'table_data' : querydata
+    }
+    return render(request,'dashboard_index.html', context)
 
 
 
-# from django.shortcuts import render
-# from django.http import HttpResponse
-
-# def dashboard(request):
-#     # Your view logic for the dashboard home page
-#     return render(request, 'dashboard_index.html')
-
-# def dashboard_profile(request, username):
-#     # Your view logic for the user profile in the dashboard
-#     return render(request, 'dashboard/profile.html', {'username': username})
